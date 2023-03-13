6675e647226402ece42b226a392dc6cdfd5cc329
MahmoudAbdelazim <mahmoud2011989@gmail.com>
2023-03-13 18:38:44 +0200

    Add Resume

--- components/Landing.js
+++ components/Landing.js
@@ -5,6 +5,8 @@ const Landing = () => {
       <h1 className="landing-name">Mahmoud Abdelazim</h1>
       <h2 className="main-bio">Junior Software Engineer</h2>
       <p className="detailed-bio">A passionate software engineer with great potential to learn, affect and grow.</p>
+      <a href="#contact" className="btn btn-primary get-in-touch-button">Get In Touch</a>
+      <a href="/files/Mahmoud_Abdelazim_s_Resume.pdf" className="btn btn-primary resume-button" download>Resume</a>
     </div>
   );
 };
